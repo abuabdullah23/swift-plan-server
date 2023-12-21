fb9ac6685013789d53e06a036c6a4e2dbbbfaf4f
abuabdullah23 <abuobaidazayed@gmail.com>
2023-12-22 03:01:21 +0600

    delete task

--- src/controllers/taskControllers/taskControllers.js
+++ src/controllers/taskControllers/taskControllers.js
@@ -19,3 +19,32 @@ exports.addNewTask = async (req, res) => {
         responseReturn(res, 500, { error: error.message })
     }
 }
+
+
+
+// get my all tasks
+exports.getMyTasks = async (req, res) => {
+    const { email } = req.params;
+
+    try {
+        const total = await TasksModel.find({ userEmail: email }).countDocuments();
+
+        const result = await TasksModel.find({ userEmail: email }).sort({ createdAt: - 1 })
+        res.send({ total, result })
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+
+// delete my tasks
+exports.deleteMyTask = async (req, res) => {
+    const taskId = req.params.id;
+
+    try {
+        await TasksModel.findByIdAndDelete(taskId);
+        responseReturn(res, 200, { message: 'Delete this article successful.' })
+    } catch (error) {
+        responseReturn(res, 500, { error: error.message })
+    }
+}
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,7 +1,10 @@
-const { addNewTask } = require('../controllers/taskControllers/taskControllers');
+const { addNewTask, getMyTasks, deleteMyTask } = require('../controllers/taskControllers/taskControllers');
 const verifyJWT = require('../middleware/verifyJWT');
 const router = require('express').Router();
 
 router.post('/add-task', verifyJWT, addNewTask);
+router.get('/get-my-tasks/:email', verifyJWT, getMyTasks);
+router.delete('/delete-my-task/:id', verifyJWT, deleteMyTask);
+
 
 module.exports = router;
